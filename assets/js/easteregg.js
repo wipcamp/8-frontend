d97b2334f66f71c887f0b80724475dae84187dd8
PINGPONGsz <chanon.thongsuk@mail.kmutt.ac.th>
2016-01-29 23:03:13 +0700

    Improve EasterEggs Part1

--- assets/js/easteregg.js
+++ assets/js/easteregg.js
@@ -1,5 +1,4 @@
 //Easter Egg?
-// So Sleepy Want to sleep so Fogive me about BAD LOGIC..
       var lock = 5;
       var key = 0;
       $(document).keydown(function(e){
@@ -21,10 +20,9 @@
             $('.easter').addClass('fadeInRight animated');
           }
 
-          //Have to use Animate.css so LAZY and SLEEPY..
           setTimeout(function(){
-            $('.easter h1').addClass('bounceIn animated');
-          },3000);
+            $('.easter .slogan').addClass('bounceIn animated');
+          },3800);
           setTimeout(function(){
             $('.easter .easter_pic_center').addClass('fadeInUp animated');
           },1900);
@@ -35,6 +33,9 @@
             $('.easter .easter_pic_linethree').addClass('fadeInUp animated');
           },2500);
           setTimeout(function(){
-            $('.easter .easter_pic_linefour').addClass('fadeInUp animated');
+            $('.easter .first').addClass('fadeInUp animated');
           },2800);
+          setTimeout(function(){
+            $('.easter .easter_pic_linefour').addClass('fadeInUp animated');
+          },3100);
       });
